43b83d2c8c1f558a6e00d78d6e38a9153a39926a
AndroEswar <androeswar39@gmail.com>
2019-09-03 00:06:02 +0530

    Updates

---
+++ precache-manifest.62fdd44e3aa2a6bd384edd5663f5e4a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f0e3b640afe0027cca0cae36e9ea82",
+    "url": "/eParch/index.html"
+  },
+  {
+    "revision": "a2f82e30a4841f403bb1",
+    "url": "/eParch/static/css/2.dc86c4cb.chunk.css"
+  },
+  {
+    "revision": "6da866bf5e6836e02ed1",
+    "url": "/eParch/static/css/main.97952eef.chunk.css"
+  },
+  {
+    "revision": "a2f82e30a4841f403bb1",
+    "url": "/eParch/static/js/2.67d53390.chunk.js"
+  },
+  {
+    "revision": "6da866bf5e6836e02ed1",
+    "url": "/eParch/static/js/main.5bee95f0.chunk.js"
+  },
+  {
+    "revision": "a49c8e03c49a8d37ef8e",
+    "url": "/eParch/static/js/runtime~main.cdb6fd42.js"
+  }
+]);
