b6487977383c042fe518d3a6ff1d64daf5b1a040
AndroEswar <androeswar39@gmail.com>
2019-09-03 00:14:42 +0530

    added build

---
+++ build/precache-manifest.7f1b00ead83a088b8405f62513e423cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965d833250d774bdf22e25143f675b5f",
+    "url": "/eParchi/index.html"
+  },
+  {
+    "revision": "569ceec102a8174e7928",
+    "url": "/eParchi/static/css/2.dc86c4cb.chunk.css"
+  },
+  {
+    "revision": "c1cd53f0a38f63232f93",
+    "url": "/eParchi/static/css/main.97952eef.chunk.css"
+  },
+  {
+    "revision": "569ceec102a8174e7928",
+    "url": "/eParchi/static/js/2.9ce7f06f.chunk.js"
+  },
+  {
+    "revision": "c1cd53f0a38f63232f93",
+    "url": "/eParchi/static/js/main.87060cc3.chunk.js"
+  },
+  {
+    "revision": "8fc097f37d67d4516839",
+    "url": "/eParchi/static/js/runtime~main.a2fb45a6.js"
+  }
+]);
